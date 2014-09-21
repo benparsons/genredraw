1877d8ecf6ab87a3084071d20c6dca0eea7b6600
Ben Parsons <ben+github@bpulse.co.uk>
2014-09-22 00:38:31 +0100

    add push to node server to save

---
+++ app.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    console.log(query);
+    var log = fs.createWriteStream('londoncalling.txt', {'flags': 'a'});
+// use {'flags': 'a'} to append and {'flags': 'w'} to erase and write a new file
+    var result = query['status'];
+    if (result)
+        log.write(result + "\n");
+    log.close();
+  res.end("");
+});
+server.listen(4040);
