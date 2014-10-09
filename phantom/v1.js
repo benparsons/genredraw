88b0b9bf9d3992702f7cdf1c8c7d505712152bae
Ben Parsons <ben+github@bpulse.co.uk>
2014-10-10 00:18:31 +0100

    change phantom to take arg

--- phantom/v1.js
+++ phantom/v1.js
@@ -1,16 +1,21 @@
 var page = require('webpage').create();
-console.log('The default user agent is ' + page.settings.userAgent);
+var system = require('system');
+if (system.args.length === 1) {
+    console.log('file path required, exiting');
+    phantom.exit();
+} else {
+    system.args.forEach(function (arg, i) {
+        console.log(i + ': ' + arg);
+    });
+}
+
 page.settings.userAgent = 'SpecialAgent';
-var url = 'http://www.httpuseragent.org';
-url = 'http://localhost:8080/colourArray2.html?filename=source/londoncalling.jpg';
+//var url = 'http://www.httpuseragent.org';
+//var url = 'http://localhost:8080/colourArray2.html?filename=source/londoncalling.jpg';
+var url = 'http://localhost:8080/colourArray2.html?filename=' + system.args[1];
 page.open(url, function(status) {
     if (status !== 'success') {
         console.log('Unable to access network');
-    } else {
-        var ua = page.evaluate(function() {
-            //return document.getElementById('myagent').textContent;
-        });
-        console.log(ua);
     }
     //phantom.exit();
 });
