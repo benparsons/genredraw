d9a7b0540a4a46a59ca379430ece95c4c2a612af
Ben Parsons <ben@bpulse.co.uk>
2014-10-26 10:22:04 +0000

    add render/renderSingle.html

--- js/util.js
+++ js/util.js
@@ -5,3 +5,9 @@ function getParameterByName(name) {
         results = regex.exec(location.search);
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+// from http://stackoverflow.com/questions/4959975/generate-random-value-between-two-numbers-in-javascript
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
